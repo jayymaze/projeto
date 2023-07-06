22185babe2957778db3c0007e8ba57432b3fbe86
jayymaze <jlmazetti03@gmail.com>
2023-07-05 21:27:54 -0300

    inicial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+function toggleMode() {
+
+  //botão de troca
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  // substituir imagem
+  // if(html.classList.constains('light')){
+  // img.setAttribute('src', './assets/avatar-light.png')
+  //} else {
+  //  img.setAttribute("src", "./assets/avatar.png") 
+  //}
+}
